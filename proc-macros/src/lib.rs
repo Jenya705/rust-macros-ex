5ea2d80a1a18419353af9b2f2f0ae0573c0e6c7e
Jenya705 <51133999+Jenya705@users.noreply.github.com>
2022-05-31 23:06:59 +0200

    Make tree printing better

--- proc-macros/src/lib.rs
+++ proc-macros/src/lib.rs
@@ -1,49 +1,48 @@
-use proc_macro::{Delimiter, Group, Ident, Literal, Punct, Spacing, Span, TokenStream, TokenTree};
-use proc_macro::Spacing::Joint;
+use quote::quote;
+use proc_macro2::{Spacing, TokenStream, TokenTree};
 
 #[proc_macro]
-pub fn print_tree_macro(body: TokenStream) -> TokenStream {
-    let mut tokens: Vec<TokenTree> = vec![];
+pub fn print_tree_macro(body: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let body = proc_macro2::TokenStream::from(body);
+    let mut streams = vec![];
 
     for tt in body.into_iter() {
-        tokens.push(TokenTree::Ident(Ident::new("println", Span::call_site())));
-        tokens.push(TokenTree::Punct(Punct::new('!', Spacing::Joint)));
-        let tt_tokens = match tt {
-            TokenTree::Ident(ident) => vec![
-                TokenTree::Literal(
-                    Literal::string("Ident: {}")
-                ),
-                TokenTree::Punct(Punct::new(',', Spacing::Joint)),
-                TokenTree::Literal(
-                    Literal::string(ident.to_string().as_str())
-                ),
-            ],
-            TokenTree::Literal(literal) => vec![
-                TokenTree::Literal(
-                    Literal::string("Literal: {}")
-                ),
-                TokenTree::Punct(Punct::new(',', Spacing::Joint)),
-                TokenTree::Literal(literal)
-            ],
-            TokenTree::Group(group) => vec![
-                TokenTree::Literal(Literal::string("Group with delimiter: {}")),
-                TokenTree::Punct(Punct::new(',', Spacing::Joint)),
-                TokenTree::Literal(Literal::string(
-                    format!("{:?}", group.delimiter()).as_str()
-                ))
-            ],
-            TokenTree::Punct(punct) => vec![
-                TokenTree::Literal(Literal::string("Punct: {}")),
-                TokenTree::Punct(Punct::new(',', Spacing::Joint)),
-                TokenTree::Literal(Literal::character(punct.as_char()))
-            ],
-        };
-        tokens.push(TokenTree::Group(Group::new(
-            Delimiter::Parenthesis,
-            TokenStream::from_iter(tt_tokens.into_iter()),
-        )));
-        tokens.push(TokenTree::Punct(Punct::new(';', Spacing::Joint)))
+        match tt {
+            TokenTree::Literal(literal) => {
+                let literal_string = literal.to_string();
+                streams.push(quote! {
+                    println!("Literal - {}", #literal_string);
+                })
+            },
+            TokenTree::Punct(punct) => {
+                let punct_char = punct.as_char();
+                let punct_spacing = match punct.spacing() {
+                    Spacing::Joint => "Joint",
+                    Spacing::Alone => "Alone",
+                };
+                streams.push(quote! {
+                    println!("Punct - '{}'; {}", #punct_char, #punct_spacing);
+                })
+            },
+            TokenTree::Ident(ident) => {
+                let ident_string = ident.to_string();
+                streams.push(quote! {
+                    println!("Ident - {}", #ident_string);
+                })
+            },
+            TokenTree::Group(group) => {
+                let group_delimiter = format!("{:?}", group.delimiter());
+                let group_stream = TokenStream::from(
+                    print_tree_macro(proc_macro::TokenStream::from(group.stream()))
+                );
+                streams.push(quote! {
+                    println!("Group - {}", #group_delimiter);
+                    #group_stream
+                    println!("End.");
+                })
+            }
+        }
     }
 
-    TokenStream::from_iter(tokens)
+    proc_macro::TokenStream::from(TokenStream::from_iter(streams))
 }
